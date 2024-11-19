ea2625251c629c69022b482d2a9ea63539062ed6
Your Name <you@example.com>
2024-11-19 21:27:18 +0000

    comments and last test

--- restock.py
+++ restock.py
@@ -1,36 +1,16 @@
 def restock_inventory(available_items, inventory_records, current_day):
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the stock/restock for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
 
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
-
-
-The function will also update the inventory_records (For restocking) for a  given current day. It will also return "available_items".
-   
-     '''
 
     restocked_units = 0
 
-    if current_day == 0:
+    if current_day == 0: # first day starts with full restock
         restocked_units = 2000
-        inventory_records.append([current_day, 0, restocked_units, available_items])     
+        inventory_records.append([current_day, 0, restocked_units, available_items])  # append the first days record
 
-    if current_day % 7 == 0:
+    if current_day % 7 == 0: # restock every 7 days
     
-        restocked_units = 2000 - available_items
-
-        available_items = 2000
-
+        restocked_units = 2000 - available_items # restock the available items to 2000 units
+        available_items = 2000 
         inventory_records.append([current_day, 0, restocked_units, available_items])      
 
     return available_items  
